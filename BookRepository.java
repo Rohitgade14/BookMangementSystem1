3720e3c85ce03702f682688c3e791b088d2f557c
Rohit Gade <155424199+Rohitgade14@users.noreply.github.com>
2024-04-01 21:16:03 +0530

    Add files via upload

---
+++ BookRepository.java
@@ -0,0 +1,9 @@
+package com.jsp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jsp.Entity1.Book1;
+
+public interface BookRepository extends CrudRepository<Book1,Integer>{
+
+}
